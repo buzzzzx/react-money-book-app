da9caae369dbc0fc8d2c7045138ac5addb39b06e
Anak7n <buzzzzx233@gmail.com>
2020-07-03 15:32:35 +0800

    feat(pricelist/app): add ionicons and do prop type check

--- src/components/PriceList.js
+++ src/components/PriceList.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Ionicon from "react-ionicons";
+import PropsType from "prop-types";
 
 const PriceList = ({ items, onModifyItem, onDeleteItem }) => (
   <ul className="list-group list-group-flush">
@@ -7,22 +9,52 @@ const PriceList = ({ items, onModifyItem, onDeleteItem }) => (
         className="list-group-item d-flex justify-content-between align-items-center"
         key={item.id}
       >
-        <span className="col-1 badge badge-primary">{item.category.name}</span>
+        <span className="col-1 badge badge-primary">
+          <Ionicon
+            className="rounded-circle"
+            fontSize="30px"
+            style={{ backgroundColor: "#007bff", padding: "5px" }}
+            color="#fff"
+            icon={item.category.iconName}
+          />
+        </span>
         <span className="col-2">{item.title}</span>
         <span className="col-5 font-weight-bold">
           {item.category.type === "income" ? "+" : "-"}
-          {item.price}
+          {item.price} 元
         </span>
         <span className="col-2">{item.date}</span>
-        <button className="btn btn-primary" onClick={() => onModifyItem(item)}>
-          编辑
-        </button>
-        <button className="btn btn-danger" onClick={() => onDeleteItem(item)}>
-          删除
-        </button>
+        <a className="col-1" onClick={() => onModifyItem(item)}>
+          <Ionicon
+            className="rounded-circle"
+            fontSize="30px"
+            style={{ backgroundColor: "#28a745", padding: "5px" }}
+            color="#fff"
+            icon="ios-create-outline"
+          />
+        </a>
+        <a className="col-1" onClick={() => onDeleteItem(item)}>
+          <Ionicon
+            className="rounded-circle"
+            fontSize="30px"
+            style={{ backgroundColor: "#dc3545", padding: "5px" }}
+            color="#fff"
+            icon="ios-trash-outline"
+          />
+        </a>
       </li>
     ))}
   </ul>
 );
+
+PriceList.propTypes = {
+  items: PropsType.array.isRequired,
+  onModifyItem: PropsType.func.isRequired,
+  onDeleteItem: PropsType.func.isRequired,
+};
+
+PriceList.defaultProps = {
+  onDeleteItem: (item) => alert("I'm default!"),
+};
 
 export default PriceList;
