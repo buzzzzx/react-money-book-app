b1e56746cfa18c833bd27b9811280e04907a25fd
Anak7n <buzzzzx233@gmail.com>
2020-07-04 23:14:21 +0800

    test(totalprice): correct the test for TotalPrice

--- src/components/__tests__/TotalPrice.test.js
+++ src/components/__tests__/TotalPrice.test.js
@@ -4,14 +4,14 @@ import { shallow } from "enzyme";
 import TotalPrice from "../TotalPrice";
 
 const props = {
-  income: 20000,
-  outcome: 4000,
+  totalIncome: 20000,
+  totalOutcome: 4000,
 };
 
 describe("test <TotalPrice />", () => {
   it("component should render correct income&outcome number", () => {
     const wrapper = shallow(<TotalPrice {...props} />);
-    expect(wrapper.find(".income span").text() * 1).toEqual(20000);
-    expect(wrapper.find(".outcome span").text() * 1).toEqual(4000);
+    expect(wrapper.find(".income").text() * 1).toEqual(20000);
+    expect(wrapper.find(".outcome").text() * 1).toEqual(4000);
   });
 });
